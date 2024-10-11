a9e3b51672bcfdbb4bc227b428d96d4600300dfb
NikBavane24 <nikhilbavane24@gmail.com>
2024-10-11 09:21:06 +0530

    Java Programs

---
+++ Assert.java
@@ -0,0 +1,14 @@
+package Practice;
+
+public class Assert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=20;
+		//assert age>=20;
+		assert age>=20:"age is below 20 now its "+age;
+		System.out.println("Age of person is "+age);
+
+	}
+
+}
---
+++ Switchcase.java
@@ -0,0 +1,22 @@
+package Practice;
+
+public class Switchcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		switch(1)
+		{
+		case 1: System.out.println("1");
+		
+		case 2: System.out.println("2");
+		break;
+		case 3: System.out.println("3");
+		break;
+		case 4: System.out.println("4");
+		break;
+		default: System.out.println("default");
+		}
+
+	}
+
+}
