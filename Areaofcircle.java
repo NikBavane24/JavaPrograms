adf65e3a828bae46dea6dfef6e0f27b636a70dd2
NikBavane24 <nikhilbavane24@gmail.com>
2024-12-10 16:41:49 +0530

    Java Programs

--- Areaofcircle.java
+++ Areaofcircle.java
@@ -6,12 +6,13 @@ public class Areaofcircle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double pi=3.14;
+		double pi=Math.PI;
+		//double pi=3.14;
 		Scanner s1=new Scanner (System.in);
 		System.out.println("Enter the value of redius=");
 		int r=               s1.nextInt();
 		double areaofcircle =pi*r*r;
-		System.out.println("Area of Circle = "+areaofcircle);
+		System.out.println("Area of Circle = "+areaofcircle); 
 	}
 
 }
